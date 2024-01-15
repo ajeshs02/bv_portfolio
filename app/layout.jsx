49991a9a606948d03e99f9a81ee05bc118e9c32e
Ajesh S <ajeshajesh9895@gmail.com>
2024-01-15 14:42:08 +0530

    favicon and seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,8 +7,8 @@ export const metadata = {
     'theme-color': '#2c1250',
     'color-scheme': 'dark only',
     'og:url': 'https://bhavya-portfolio-iota.vercel.app/',
-    // 'og:image':
-    //   'https://cdn.sanity.io/images/stq2zs7v/production/f5f70d04a0a726c422bc61dc6422f6638846364c-1801x1080.webp',
+    'og:image':
+      'https://drive.google.com/file/d/1lJl5tl7HbZzNj7MA6S5NK1cplIv4l7e2/view?usp=sharing',
     'og:type': 'website',
   },
 }
